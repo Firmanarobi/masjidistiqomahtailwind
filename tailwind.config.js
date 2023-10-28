280f5241b951ab564f72f80d5869da1d188202aa
unknown <zaynfirman2@gmail.com>
2023-10-28 22:09:53 +0700

    landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['index.html','home.html','home1.html'],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors:{
+        primary: '#1D5883',
+        secondary: '#047F6C',
+        dark: '#0f172a'
+      },
+      screens: {
+        '2xl': '1320px'
+      }
+    },
   },
   plugins: [],
 }
